ce92b3b92dfbcbc7f83bbc9aaa08e44f8462b122
Rebecca Chang Swee Fun <rebecca.chang@starfivetech.com>
2022-11-14 00:46:42 -0500

    linux_syscall_support.h: fix chromium build issue on signed-conversion
    Signed-off-by: Rebecca Chang Swee Fun <rebecca.chang@starfivetech.com>

--- linux_syscall_support.h
+++ linux_syscall_support.h
@@ -3498,7 +3498,7 @@ struct kernel_statfs {
       int64_t __res;
       {
         register int64_t __res_a0 __asm__("a0");
-        register uint64_t __flags __asm__("a0") = flags;
+        register int64_t __flags __asm__("a0") = flags;
         register void *__stack __asm__("a1") = child_stack;
         register void *__ptid  __asm__("a2") = parent_tidptr;
         register void *__tls   __asm__("a3") = newtls;
